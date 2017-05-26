f56a44cd7573bb7159aa029bbdbcc7fb4a00d450
grjohn <grjohn@126.com>
2017-05-26 22:29:41 +0800

    动态显示站场界面和按钮排列车辆无法同时实现，简直fuck，明天重新设计

--- TDCS/src/com/blocklist/连锁表.java
+++ TDCS/src/com/blocklist/连锁表.java
@@ -4,9 +4,10 @@ import java.util.Calendar;
 import java.util.GregorianCalendar; 
 import com.model.*;
 import com.execution.执行窗体;
+import com.execution.执行窗体.站场图;
 
 public class 连锁表 {
-	public final long time=10;			//列车的行驶速度时间
+	public final long time=2000;			//列车的行驶速度时间
 	public 调车信号机 d1;
 	public 出站兼调车信号机 s1,s2;
 	public 进站信号机 x;
@@ -40,30 +41,31 @@ public class 连锁表 {
             return second;  
         }  
       
-	public String zhengxianjiecheX_S2() throws InterruptedException{   //正线接车操作，不涉及画图
+	public String zhengxianjiecheX_S2(执行窗体 j) throws InterruptedException{   //正线接车操作，涉及画图的页面刷新
 		int a1=adg.getblock()+dc3.getblock()+dg2.getblock()+dg3.getblock();  //锁闭条件
 		int a2=adg.getclean()+dg3.getclean()+dg2.getclean();  //空闲条件
 		System.out.println(a1+" "+a2);
 		if(a1==0&&a2==3){
+			j.repaint();Thread.sleep(time);	
 			dc3.setdaily(1);			//定位状态
 			adg.setblock(1);			//锁闭
 			dc3.setblock(1);
 			dg3.setblock(1);
 			dg2.setblock(1);setCurrentTime();
 			System.out.println(hour+":"+minute+":"+second+"---"+"正线接车成功办理！列车开始行驶");
-			x.setcolor(Color.yellow);d1.setcolor(Color.gray);  //灯光颜色转换
+			x.setcolor(Color.yellow);d1.setcolor(Color.gray);j.repaint();  //灯光颜色转换
 			Thread.sleep(2*time);
-			adg.setclean(0);x.setcolor(Color.red);		//列车进入adg
+			adg.setclean(0);x.setcolor(Color.red);j.repaint();		//列车进入adg
 			Thread.sleep(time);						
-			xdg.setblock(0);						//列车出清xdg，自动解锁
+			xdg.setblock(0);j.repaint();						//列车出清xdg，自动解锁
 			Thread.sleep(time);
-			dg3.setclean(0);						//列车进入dg3
+			dg3.setclean(0);j.repaint();						//列车进入dg3
 			Thread.sleep(time);
-			adg.setblock(0);						//列车出清adg，自动解锁	
+			adg.setblock(0);j.repaint();						//列车出清adg，自动解锁	
 			Thread.sleep(time);
-			dg2.setclean(0);						//列车占用dg2
+			dg2.setclean(0);j.repaint();						//列车占用dg2
 			Thread.sleep(time);
-			dg3.setblock(0);dc3.setblock(0);dg2.setblock(0);dg2.setclean(0);d1.setcolor(Color.blue);setCurrentTime();		//列车出清dg3,dg3,dc3,dg2同时解锁,列车到达dg2
+			dg3.setblock(0);dc3.setblock(0);dg2.setblock(0);dg2.setclean(0);d1.setcolor(Color.blue);setCurrentTime();j.repaint();		//列车出清dg3,dg3,dc3,dg2同时解锁,列车到达dg2
 			return hour+":"+minute+":"+second+"---"+"列车成功正线到达股道。";
 		}
 		
@@ -73,26 +75,27 @@ public class 连锁表 {
 				return hour+":"+minute+":"+second+"---"+"不可以办理正线接车";		
 			}
 	}
-	public String diaoched1_S2() throws InterruptedException{   //d1至s2调车操作，不涉及画图
+	public String diaoched1_S2(执行窗体 j) throws InterruptedException{   //d1至s2调车操作，涉及画图的页面刷新
 		int a1=dc3.getblock()+dg3.getblock();  //锁闭条件
 		int a2=dg3.getclean();  //空闲条件
 		System.out.println(a1+" "+a2);
 		if(a1==0&&a2==1){
+			j.repaint();Thread.sleep(time);	
 			dc3.setdaily(1);			//定位状态
 						//锁闭
 			dc3.setblock(1);
 			dg3.setblock(1);
-			dg2.setblock(1);setCurrentTime();
+			dg2.setblock(1);setCurrentTime();j.repaint();
 			System.out.println(hour+":"+minute+":"+second+"---"+"d1至s2调车成功办理！列车开始行驶");
-			d1.setcolor(Color.white);  //灯光颜色转换
+			d1.setcolor(Color.white);j.repaint();  //灯光颜色转换
 			Thread.sleep(2*time);
-			dg3.setclean(0);						//列车进入dg3
+			dg3.setclean(0);j.repaint();						//列车进入dg3
 			Thread.sleep(time);
-			adg.setblock(0);d1.setcolor(Color.blue);						//列车出清adg，自动解锁，调车信号机变蓝
+			adg.setblock(0);d1.setcolor(Color.blue);j.repaint();						//列车出清adg，自动解锁，调车信号机变蓝
 			Thread.sleep(time);
-			dg2.setclean(0);						//列车占用dg2
+			dg2.setclean(0);j.repaint();						//列车占用dg2
 			Thread.sleep(time);
-			dg3.setblock(0);dc3.setblock(0);dg2.setblock(0);dg2.setclean(0);setCurrentTime();		//列车出清dg3,dg3,dc3同时解锁,列车到达dg2
+			dg3.setblock(0);dc3.setblock(0);dg2.setblock(0);dg2.setclean(0);setCurrentTime();j.repaint();		//列车出清dg3,dg3,dc3同时解锁,列车到达dg2
 			return hour+":"+minute+":"+second+"---"+"列车成功由d1至s2调车。";
 		}
 		
@@ -103,30 +106,31 @@ public class 连锁表 {
 			}
 	}
 	
-	public String facheches1_x() throws InterruptedException{   //侧线发车操作，不涉及画图
+	public String facheches1_x(执行窗体 j) throws InterruptedException{   //侧线发车操作，涉及画图的页面刷新
 		int a1=adg.getblock()+dc3.getblock()+xdg.getblock()+dg3.getblock();  //锁闭条件
 		int a2=adg.getclean()+dg3.getclean()+xdg.getclean();  //空闲条件
 		System.out.println(a1+" "+a2);
 		if(a1==0&&a2==3){
+			j.repaint();Thread.sleep(time);	
 			dc3.setdaily(0);			//定位状态
 			adg.setblock(1);			//锁闭
 			dc3.setblock(1);
 			dg3.setblock(1);
-			xdg.setblock(1);setCurrentTime();
+			xdg.setblock(1);setCurrentTime();j.repaint();
 			System.out.println(hour+":"+minute+":"+second+"---"+"s1发车成功办理！列车开始行驶");
-			s1.setcolor(Color.yellow);d1.setcolor(Color.gray);  //灯光颜色转换
+			s1.setcolor(Color.yellow);d1.setcolor(Color.gray);j.repaint();  //灯光颜色转换
 			Thread.sleep(2*time);
-			dg3.setclean(0);s1.setcolor(Color.red);		//列车进入dg3
+			dg3.setclean(0);s1.setcolor(Color.red);j.repaint();		//列车进入dg3
 			Thread.sleep(time);						
-			dg1.setblock(0);						//列车出清dg1，自动解锁
+			dg1.setblock(0);j.repaint();						//列车出清dg1，自动解锁
 			Thread.sleep(time);
-			adg.setclean(0);						//列车进入adg
+			adg.setclean(0);j.repaint();						//列车进入adg
 			Thread.sleep(time);
-			dg3.setblock(0);dc3.setblock(0);			//列车出清dg3，自动解锁	
+			dg3.setblock(0);dc3.setblock(0);j.repaint();			//列车出清dg3，自动解锁	
 			Thread.sleep(time);
-			xdg.setclean(0);						//列车占用xdg
+			xdg.setclean(0);j.repaint();						//列车占用xdg
 			Thread.sleep(time);
-			xdg.setblock(0);d1.setcolor(Color.blue);setCurrentTime();		//列车出清xdg同时解锁,列车到达dg2
+			xdg.setblock(0);d1.setcolor(Color.blue);setCurrentTime();j.repaint();		//列车出清xdg同时解锁,列车到达dg2
 			return hour+":"+minute+":"+second+"---"+"列车成功由s1驶离出站。";
 		}
 		
@@ -137,27 +141,28 @@ public class 连锁表 {
 			}
 	}
 	
-	public String diaoches1_d1() throws InterruptedException{   //s1至d1调车操作，不涉及画图
+	public String diaoches1_d1(执行窗体 j) throws InterruptedException{   //s1至d1调车操作，涉及画图的页面刷新
 		int a1=dc3.getblock()+dg3.getblock();  //锁闭条件
 		int a2=dg3.getclean();  //空闲条件
 		System.out.println(a1+" "+a2);
 		if(a1==0&&a2==1){
+			j.repaint();Thread.sleep(time);	
 			dc3.setdaily(0);			//定位状态
 			adg.setblock(1);			//锁闭
 			dc3.setblock(1);
 			dg3.setblock(1);
-			setCurrentTime();
+			setCurrentTime();j.repaint();
 			System.out.println(hour+":"+minute+":"+second+"---"+"s1至d1调车成功办理！列车开始行驶");
-			s1.setcolor(Color.white);d1.setcolor(Color.red);  //灯光颜色转换
+			s1.setcolor(Color.white);d1.setcolor(Color.red);j.repaint();  //灯光颜色转换
 			Thread.sleep(2*time);
-			dg3.setclean(0);	//列车进入dg3
+			dg3.setclean(0);j.repaint();	//列车进入dg3
 			Thread.sleep(time);						
-			dg1.setblock(0);s1.setcolor(Color.red);							//列车出清dg1，自动解锁
+			dg1.setblock(0);s1.setcolor(Color.red);	j.repaint();						//列车出清dg1，自动解锁
 			Thread.sleep(time);
-			adg.setclean(0);						//列车进入adg
+			adg.setclean(0);j.repaint();						//列车进入adg
 			Thread.sleep(time);
 			dg3.setblock(0);dc3.setblock(0);adg.setblock(0);			//列车出清dg3，自动解锁	
-			d1.setcolor(Color.blue);setCurrentTime();		//列车出清xdg同时解锁,列车到达dg2
+			d1.setcolor(Color.blue);setCurrentTime();j.repaint();		//列车出清xdg同时解锁,列车到达dg2
 			return hour+":"+minute+":"+second+"---"+"列车成功由s1至d1调车。";
 		}
 		
@@ -168,30 +173,31 @@ public class 连锁表 {
 			}
 	}
 	
-	public String facheches2_x() throws InterruptedException{   //正线发车操作，不涉及画图
+	public String facheches2_x(执行窗体 j) throws InterruptedException{   //正线发车操作，涉及画图的页面刷新
 		int a1=adg.getblock()+dc3.getblock()+xdg.getblock()+dg3.getblock();  //锁闭条件
 		int a2=adg.getclean()+dg3.getclean()+xdg.getclean();  //空闲条件
 		System.out.println(a1+" "+a2);
 		if(a1==0&&a2==3){
+			j.repaint();Thread.sleep(time);	
 			dc3.setdaily(1);			//定位状态
 			adg.setblock(1);			//锁闭
 			dc3.setblock(1);
 			dg3.setblock(1);
-			xdg.setblock(1);setCurrentTime();
+			xdg.setblock(1);setCurrentTime();j.repaint();
 			System.out.println(hour+":"+minute+":"+second+"---"+"s2发车成功办理！列车开始行驶");
-			s2.setcolor(Color.yellow);d1.setcolor(Color.gray);  //灯光颜色转换
+			s2.setcolor(Color.yellow);d1.setcolor(Color.gray);j.repaint();  //灯光颜色转换
 			Thread.sleep(2*time);
-			dg3.setclean(0);s2.setcolor(Color.red);		//列车进入dg3
+			dg3.setclean(0);s2.setcolor(Color.red);j.repaint();		//列车进入dg3
 			Thread.sleep(time);						
-			dg2.setblock(0);						//列车出清dg2，自动解锁
+			dg2.setblock(0);j.repaint();						//列车出清dg2，自动解锁
 			Thread.sleep(time);
-			adg.setclean(0);						//列车进入adg
+			adg.setclean(0);j.repaint();						//列车进入adg
 			Thread.sleep(time);
-			dg3.setblock(0);dc3.setblock(0);			//列车出清dg3，自动解锁	
+			dg3.setblock(0);dc3.setblock(0);j.repaint();			//列车出清dg3，自动解锁	
 			Thread.sleep(time);
-			xdg.setclean(0);						//列车占用xdg
+			xdg.setclean(0);j.repaint();						//列车占用xdg
 			Thread.sleep(time);
-			xdg.setblock(0);d1.setcolor(Color.blue);setCurrentTime();		//列车出清xdg同时解锁,列车到达dg2
+			xdg.setblock(0);d1.setcolor(Color.blue);setCurrentTime();j.repaint();		//列车出清xdg同时解锁,列车到达dg2
 			return hour+":"+minute+":"+second+"---"+"列车成功由s2驶离出站。";
 		}
 		
@@ -202,26 +208,27 @@ public class 连锁表 {
 			}
 	}
 	
-	public String diaoches2_d1() throws InterruptedException{   //s2至d1调车操作，不涉及画图
+	public String diaoches2_d1(执行窗体 j) throws InterruptedException{   //s2至d1调车操作，涉及画图的页面刷新
 		int a1=dc3.getblock()+dg3.getblock();  //锁闭条件
 		int a2=dg3.getclean();  //空闲条件
 		System.out.println(a1+" "+a2);
 		if(a1==0&&a2==1){
+			j.repaint();Thread.sleep(time);	
 			dc3.setdaily(1);			//定位状态
 			dc3.setblock(1);
 			dg3.setblock(1);
-			setCurrentTime();
+			setCurrentTime();j.repaint();
 			System.out.println(hour+":"+minute+":"+second+"---"+"s2至d1调车办理！列车开始行驶");
-			s2.setcolor(Color.white);d1.setcolor(Color.red);  //灯光颜色转换
+			s2.setcolor(Color.white);d1.setcolor(Color.red);j.repaint();  //灯光颜色转换
 			Thread.sleep(2*time);
-			dg3.setclean(0);s2.setcolor(Color.red);		//列车进入dg3
+			dg3.setclean(0);s2.setcolor(Color.red);	j.repaint();	//列车进入dg3
 			Thread.sleep(time);						
-			dg2.setblock(0);						//列车出清dg2，自动解锁
+			dg2.setblock(0);j.repaint();						//列车出清dg2，自动解锁
 			Thread.sleep(time);
-			adg.setclean(0);						//列车进入adg
+			adg.setclean(0);j.repaint();						//列车进入adg
 			Thread.sleep(time);
 			dg3.setblock(0);dc3.setblock(0);adg.setblock(0);			//列车出清dg3，自动解锁	
-			d1.setcolor(Color.blue);setCurrentTime();		//列车出清xdg同时解锁,列车到达dg2
+			d1.setcolor(Color.blue);setCurrentTime();j.repaint();		//列车出清xdg同时解锁,列车到达dg2
 			return hour+":"+minute+":"+second+"---"+"列车成功由s2至d1调车。";
 		}
 		
@@ -242,30 +249,31 @@ public class 连锁表 {
 		xdg=new 股道();xdg.setclean(0);	
 	}
 	
-	public String cexianjiecheX_S1() throws InterruptedException{   //侧线接车操作，不涉及画图
+	public String cexianjiecheX_S1(执行窗体 j) throws InterruptedException{   //侧线接车操作，涉及画图的页面刷新
 		int a1=adg.getblock()+dc3.getblock()+dg1.getblock()+dg3.getblock();  //锁闭条件
 		int a2=adg.getclean()+dg3.getclean()+dg1.getclean();  //空闲条件
 		System.out.println(a1+" "+a2);
 		if(a1==0&&a2==3){
+			j.repaint();Thread.sleep(time);	
 			dc3.setdaily(0);			//定位状态
 			adg.setblock(1);			//锁闭
 			dc3.setblock(1);
 			dg3.setblock(1);
-			dg1.setblock(1);setCurrentTime();
+			dg1.setblock(1);setCurrentTime();j.repaint();
 			System.out.println(hour+":"+minute+":"+second+"---"+"侧线接车成功办理！列车开始行驶");
-			x.setcolor(Color.YELLOW);d1.setcolor(Color.gray);  //灯光颜色转换
+			x.setcolor(new Color(255,255,1,255));d1.setcolor(Color.gray);j.repaint();  //灯光颜色转换
 			Thread.sleep(2*time);
-			adg.setclean(0);x.setcolor(Color.red);		//列车进入adg
+			adg.setclean(0);x.setcolor(Color.red);j.repaint();		//列车进入adg
 			Thread.sleep(time);						
-			xdg.setblock(0);						//列车出清xdg，自动解锁
+			xdg.setblock(0);j.repaint();						//列车出清xdg，自动解锁
 			Thread.sleep(time);
-			dg3.setclean(0);						//列车进入dg3
+			dg3.setclean(0);j.repaint();						//列车进入dg3
 			Thread.sleep(time);
-			adg.setblock(0);						//列车出清adg，自动解锁	
+			adg.setblock(0);j.repaint();						//列车出清adg，自动解锁	
 			Thread.sleep(time);
-			dg1.setclean(0);						//列车占用dg2
+			dg1.setclean(0);j.repaint();						//列车占用dg2
 			Thread.sleep(time);
-			dg3.setblock(0);dc3.setblock(0);dg1.setblock(0);dg1.setclean(0);d1.setcolor(Color.blue);setCurrentTime();		//列车出清dg3,dg3,dc3,dg2同时解锁,列车到达dg2
+			dg3.setblock(0);dc3.setblock(0);dg1.setblock(0);dg1.setclean(0);d1.setcolor(Color.blue);setCurrentTime();j.repaint();		//列车出清dg3,dg3,dc3,dg2同时解锁,列车到达dg2
 			return hour+":"+minute+":"+second+"---"+"列车成功侧线到达股道。";
 		}
 		else
@@ -275,26 +283,27 @@ public class 连锁表 {
 			}
 	}
 	
-	public String diaoched1_S1() throws InterruptedException{   //d1至s1调车操作，不涉及画图
+	public String diaoched1_S1(执行窗体 j) throws InterruptedException{   //d1至s1调车操作，涉及画图的页面刷新
 		int a1=dc3.getblock()+dg3.getblock();  //锁闭条件
 		int a2=dg3.getclean();  //空闲条件
 		System.out.println(a1+" "+a2);
 		if(a1==0&&a2==1){
+			j.repaint();Thread.sleep(time);	
 			dc3.setdaily(0);			//定位状态
 			adg.setblock(1);			//锁闭
 			dc3.setblock(1);
 			dg3.setblock(1);
-			dg1.setblock(1);setCurrentTime();
+			dg1.setblock(1);setCurrentTime();j.repaint();
 			System.out.println(hour+":"+minute+":"+second+"---"+"d1至s1调车成功办理！列车开始行驶");
-			d1.setcolor(Color.white);  //灯光颜色转换
+			d1.setcolor(Color.white);j.repaint();  //灯光颜色转换j.repaint();
 			Thread.sleep(time);
-			dg3.setclean(0);						//列车进入dg3
+			dg3.setclean(0);j.repaint();				//列车进入dg3
 			Thread.sleep(time);
-			adg.setblock(0);						//列车出清adg，自动解锁	
+			adg.setblock(0);j.repaint();					//列车出清adg，自动解锁	
 			Thread.sleep(time);
-			dg1.setclean(0);						//列车占用dg1
+			dg1.setclean(0);j.repaint();				//列车占用dg1
 			Thread.sleep(time);
-			dg3.setblock(0);dc3.setblock(0);dg1.setblock(0);dg1.setclean(0);d1.setcolor(Color.blue);setCurrentTime();		//列车出清dg3,dg3,dc3,dg2同时解锁,列车到达dg2
+			dg3.setblock(0);dc3.setblock(0);dg1.setblock(0);dg1.setclean(0);d1.setcolor(Color.blue);setCurrentTime();	j.repaint();	//列车出清dg3,dg3,dc3,dg2同时解锁,列车到达dg2
 			return hour+":"+minute+":"+second+"---"+"列车成功由d1至s1调车。";
 		}
 		else
--- TDCS/src/com/execution/执行窗体.java
+++ TDCS/src/com/execution/执行窗体.java
@@ -1,70 +1,103 @@
 package com.execution;
 import com.blocklist.*;
+
 import java.awt.BasicStroke;
 import java.awt.Color;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Line2D;
+import java.awt.geom.Line2D;  
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class 执行窗体 extends JFrame{
 	
 	
-	private 连锁表 l1=new 连锁表();
-	private 站场图 zhanchangtu=new 站场图(l1);
-	
+	public 连锁表 l1=new 连锁表();
+	public 站场图 zhanchangtu=new 站场图(l1);
 	
 	public 执行窗体() throws InterruptedException{											//整体窗口的构造函数
-	this.setTitle("北京西站站场图(局部)");
-	this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
-	this.setSize(1200, 900);
-	this.setVisible(true);
-	JPanel states = new JPanel();
-	states.setBackground(new Color(240, 240, 240));
-	this.getContentPane().add(states, BorderLayout.NORTH);
-	states.setLayout(new BorderLayout(0, 0));
-	
-	JTextArea textstates = new JTextArea(6,30);
-	textstates.setFont(new Font("微软雅黑", Font.PLAIN, 17));
-	textstates.setLineWrap(true);
-	states.add(textstates);
-	this.getContentPane().add(zhanchangtu, BorderLayout.CENTER);
-//	textstates.setText(l1.diaoched1_S1()+"\r\n");
-	textstates.append(l1.diaoched1_S2());
-	
-	
-	JPanel panel_2 = new JPanel();
-	this.getContentPane().add(panel_2, BorderLayout.SOUTH);
+		this.setTitle("北京西站站场图(局部)");
+		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+		this.setSize(1200, 900);
+		this.setVisible(true);
+		JPanel states = new JPanel();
+		states.setBackground(new Color(240, 240, 240));
+		this.getContentPane().add(states, BorderLayout.NORTH);
+		states.setLayout(new BorderLayout(0, 0));
+		
+		JTextArea textstates = new JTextArea(6,30);
+		textstates.setFont(new Font("微软雅黑", Font.PLAIN, 17));
+		textstates.setLineWrap(true);
+		states.add(textstates);
+		this.getContentPane().add(zhanchangtu, BorderLayout.CENTER);
+//		textstates.setText(l1.diaoched1_S1()+"\r\n");
+		textstates.append(l1.diaoched1_S2(this));	
+//	public 执行窗体() throws InterruptedException{											//整体窗口的构造函数
+//		this.setTitle("北京西站站场图(局部)");
+//		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+//		this.setSize(1200, 900);
+//		this.setVisible(true);
+//		JPanel states = new JPanel();
+//		this.repaint();
+//		states.setBackground(new Color(240, 240, 240));
+//		this.getContentPane().add(states, BorderLayout.SOUTH);
+//		states.setLayout(new BorderLayout(0, 0));
+//		
+//		JPanel panel = new JPanel();
+//		states.add(panel);
+//		
+//		JTextArea textstates = new JTextArea(6,30);
+//		panel.add(textstates);
+//		textstates.setFont(new Font("微软雅黑", Font.PLAIN, 17));
+//		textstates.setLineWrap(true);
+//		
+////		JComboBox cbdiaoche = new JComboBox();
+////		panel.add(cbdiaoche);
+////		cbdiaoche.setModel(new DefaultComboBoxModel(new String[] {"d1\u81F3s1", "d1\u81F3s2", "s1\u81F3d1", "s2\u81F3d2"}));
+////		zhanchangtu.repaint();
+////		JComboBox cblieche = new JComboBox();
+////		panel.add(cblieche);
+////
+////		cblieche.setModel(new DefaultComboBoxModel(new String[] {"x\u81F3s1", "x\u81F3s2", "s1\u81F3x", "s2\u81F3x"}));
+//				textstates.setText(l1.diaoched1_S1(this)+"\r\n");
+////				textstates.append("1234");
+//		this.getContentPane().add(zhanchangtu, BorderLayout.CENTER);
+//	
+//	//repaint();
+//	//textstates.append(l1.diaoched1_S2());	
+//	}
 	}
-	
-	
-	
+
 	
 	public static void main(String[] args) throws InterruptedException{
-		new 执行窗体();
+		执行窗体 v1=new 执行窗体();
 	}
 	
-	class 站场图 extends JPanel{
+	public class 站场图 extends JPanel{
 	
 	public 站场图(连锁表 l1){
 		System.out.println("1st");
+
+		this.setBackground(Color.green);
 	}
 		
-	public void paint(Graphics g2) {
+	public void paint(Graphics g2) {				//整个场站图的绘制
 		super.paint(g2);
-		float lny=80;float inx=190;int lnty=80;int intx=190;
+		float lny=50;float inx=200;int lnty=50;int intx=200;
 		Graphics2D g=(Graphics2D)g2;
 		BasicStroke stroke=new BasicStroke(5,BasicStroke.CAP_ROUND,BasicStroke.JOIN_BEVEL);
 		g.setStroke(stroke);
 		g.setFont(new Font("微软雅黑", Font.PLAIN, 20));
-		System.out.println(l1.dg1.getcolor());
 		
 		Line2D.Float line=new Line2D.Float(0+inx, 190+lny, 0+inx+80, 190+lny);			//xdg
 		g.setColor(l1.xdg.getcolor());
 		g.draw(line);g.drawString("xdg", 30+intx, 210+lnty);
 
-		g.setColor(l1.x.getcolor());//x
+		g.setColor(l1.x.getcolor());System.out.println(l1.x.getcolor());							//x
 		g.fillOval(83+intx, 150+lnty, 40,40);
-		if(l1.x.getcolor()==Color.YELLOW) g.fillOval(123+intx, 150+lnty, 40, 40);
+		if(l1.x.getcolor()==new Color(255,255,1,255)) g.fillOval(123+intx, 150+lnty, 40, 40);
 		
 		line=new Line2D.Float(80+inx, 190+lny, 280+inx, 190+lny);	//adg
 		g.setColor(l1.adg.getcolor());
